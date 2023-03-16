816584f0687af3f8a452cd6deb450a8df851bc0b
Filip Zolyniak <72789688+Zoltw@users.noreply.github.com>
2023-03-17 00:43:13 +0100

    added early frontend routing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,32 @@ import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@pages/App/App';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <div>404</div>,
+  },
+  {
+    path: '/signup',
+    // element: <Signup />,
+    errorElement: <App />,
+  },
+  {
+    path: '/login',
+    // element: <Login />,
+    errorElement: <App />,
+  },
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <Suspense fallback={<div>Loading ...</div>}>
+  <Suspense fallback={< div>Loading ...</div>}>
     <React.StrictMode>
-      <App />
+      <RouterProvider router={router} />
     </React.StrictMode>
   </Suspense>,
 );
