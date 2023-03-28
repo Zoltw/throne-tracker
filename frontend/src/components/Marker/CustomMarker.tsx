8d4e5b5baf67a437f37255cb4e31e13749b881ee
Filip Zolyniak <72789688+Zoltw@users.noreply.github.com>
2023-03-28 15:09:59 +0200

    fixed hiding markers problem

--- frontend/src/components/Marker/CustomMarker.tsx
+++ frontend/src/components/Marker/CustomMarker.tsx
@@ -1,40 +1,74 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { markersExample } from './ExampleData';
 import { InfoWindow, Marker } from '@react-google-maps/api';
 import customMarker from '@assets/customMarker.svg';
 import Button from '@components/Button/Button';
 import style from './CustomMarker.module.css';
 
-export type MarkerType = {
+type MarkerType = {
   location: google.maps.LatLngLiteral;
   name: string;
+  visible: boolean;
+  __mapMarker?: google.maps.Marker;
 };
 
 export default function CustomMarker(): JSX.Element {
-  const [selectedMarker, setSelectedMarker] = useState<MarkerType | null>();
+  const [selectedMarker, setSelectedMarker] = useState<MarkerType | null>(null);
+  const [markers, setMarkers] = useState<MarkerType[]>([]);
+  const mapRef = useRef<google.maps.Map | null>(null);
+
+  useEffect(() => {
+    const initialMarkers = markersExample.map((marker) => ({
+      ...marker,
+      visible: true,
+    }));
+    setMarkers(initialMarkers);
+  }, []);
+
+  useEffect(() => {
+    markers.forEach((marker) => {
+      const map = mapRef.current;
+      const existingMarker = marker.__mapMarker;
+
+      if (existingMarker && !marker.visible) {
+        existingMarker.setMap(null);
+        delete marker.__mapMarker;
+      } else if (map && !existingMarker && marker.visible) {
+        const newMarker = new google.maps.Marker({
+          position: marker.location,
+          map,
+          icon: customMarker,
+        });
+        marker.__mapMarker = newMarker;
+      }
+    });
+  }, [markers]);
+
+  const handleMarkerClick = (marker: MarkerType) => {
+    setSelectedMarker(marker);
+  };
+
+  const handleInfoWindowClose = () => {
+    setSelectedMarker(null);
+  };
 
   return (
     <>
-      {markersExample.map((marker) => {
-        return (
-          <div key={marker.name}>
+      {markers.map((marker) => (
+        <div key={marker.name}>
+          {marker.visible && (
             <Marker
               position={marker.location}
-              options={{ icon: customMarker }}
-              onClick={() => {
-                setSelectedMarker(marker);
-              }}
+              icon={customMarker}
+              onClick={() => handleMarkerClick(marker)}
             />
-          </div>
-        );
-      })}
+          )}
+        </div>
+      ))}
       {selectedMarker && (
         <InfoWindow
           position={selectedMarker.location}
-          options={{ maxWidth: 200 }}
-          onCloseClick={() => {
-            setSelectedMarker(null);
-          }}
+          onCloseClick={handleInfoWindowClose}
         >
           <div className={style.smallBox}>
             <span>{selectedMarker.name}</span>
